0e267da43f781a2e2f4fc2743f66b7770a6b1855
KlebersonDuarte <klebersonduartesantos39@gmail.com>
2026-08-18 20:51:57 -0300

    feat: adicionando cadastro de alunos

---
+++ BioFrequencia/Model/Classroom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BioFrequencia.Model
+{
+    internal class Class
+    {
+        public string nome_sala { get; set; }
+        public string nome { get; set; }
+    }
+}
